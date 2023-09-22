2a1a149cf331608f5d50706a0b9ed7e2764c4dd6
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-22 17:43:43 +0530

    Adding the notification component to show messgaes but its not working properly at present.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import Header from './Header';
 import ProductList from './ProductList';
-import {useSelector} from 'react-redux';
-
+import {useSelector, useDispatch} from 'react-redux';
+import Notification from './Notification';
+import { UiActions } from './store/ui-slice';
 
 function App() {
   const cart = useSelector((state)=>state.cart.items);
+  const notification = useSelector((state)=>state.ui.showNotification);
+  console.log('notification:', notification);
+  const [isInitial, setIsInitial] = useState(notification===null);
+  const dispatch = useDispatch();
+  useEffect(() => {
 
-  useEffect(()=>{
-    fetch('https://reduxshoppingapp-b6011-default-rtdb.firebaseio.com/Cart.json', {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    })
-  },[cart]);
+
+    const sendCartData = async () => {
+  
+      
+    dispatch(UiActions.showNotification({
+      status: "pending",
+      title: "Sending...",
+      message: "Sending cart data!",
+    }));
+
+    try {
+      const response = await fetch("https://reduxshoppingapp-b6011-default-rtdb.firebaseio.com/Cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      } else {
+        dispatch(UiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully",
+        }));
+      }
+    } catch (error) {
+      dispatch(
+        UiActions.showNotification({
+          status: "failed",
+          title: "Failed!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+
+  if (!isInitial) {
+      sendCartData();
+    } else {
+      setIsInitial(false);
+  }
+    
+}, [cart, dispatch]);
 
 
   return (
     <div>
       <Header/>
+      <Fragment>
+        {notification && <Notification status={notification.status}
+        title={notification.title} message={notification.message}
+        />}
+      </Fragment>
       <ProductList/>
     </div>
   );
 }
+
 
 export default App;
