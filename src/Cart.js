00d7cfd15ae2b177daf5f30d4653d5bd5504ebbf
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-09-22 12:11:58 +0530

    Making the proper css and cart functionality to add or remove quantity.

--- src/Cart.js
+++ src/Cart.js
@@ -1,38 +1,84 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
+import { Modal, Button, Toast } from 'react-bootstrap';
 
 const Cart = () => {
-  const [showModal, setShowModal] = useState(true); // Initially set to true to show the cart
+  const [showModal, setShowModal] = useState(true);
+  const [showToast, setShowToast] = useState(true);
+  const [cartItems, setCartItems] = useState([
+    { id: 1, name: 'Product 1', price: 20, quantity: 1 },
+    { id: 2, name: 'Product 2', price: 30, quantity: 1 },
+    { id: 3, name: 'Product 3', price: 25, quantity: 1 }
+  ]);
 
   const toggleModal = () => {
     setShowModal(!showModal);
   };
 
-  const cartItems = [
-    { id: 1, name: 'Product 1', price: 20 },
-    { id: 2, name: 'Product 2', price: 30 },
-    { id: 3, name: 'Product 3', price: 25 }
-  ]; 
+  const getTotalPrice = () => {
+    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  };
+
+  const handleAddItem = (itemId) => {
+    const updatedCart = cartItems.map(item =>
+      item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setCartItems(updatedCart);
+  };
+
+  const handleRemoveItem = (itemId) => {
+    const updatedCart = cartItems.map(item =>
+      item.id === itemId ? { ...item, quantity: item.quantity - 1 } : item
+    );
+    setCartItems(updatedCart);
+  };
 
   return (
-    <Modal show={showModal} onHide={toggleModal} backdrop="static" keyboard={false}>
-      <Modal.Header closeButton>
-        <Modal.Title>Your Cart</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <ul className="list-group">
-          {cartItems.map(item => (
-            <li key={item.id} className="list-group-item">
-              {item.name} - Rs. {item.price}
-            </li>
-          ))}
-        </ul>
-      </Modal.Body>
-      <Modal.Footer>
-        <p>Total: Rs. {cartItems.reduce((total, item) => total + item.price, 0)}</p>
-        <button className="btn btn-primary">Checkout</button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        delay={3000} 
+        autohide
+        style={{
+          position: 'fixed',
+          top: '20px',
+          right: '20px',
+          backgroundColor: '#28a745',
+          color: '#fff',
+          zIndex: 9999 // Place the toast above everything else
+        }}
+      >
+        <Toast.Body>Your cart is opened. Happy shopping!</Toast.Body>
+      </Toast>
+
+      <Modal show={showModal} onHide={toggleModal} backdrop="static" keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <span>Cart</span> 
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <ul className="list-group">
+            {cartItems.map(item => (
+              <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+                <div>
+                  {item.name} - Rs. {item.price} x {item.quantity}
+                </div>
+                <div>
+                  <Button variant="success" onClick={() => handleAddItem(item.id)}>+</Button>
+                  <span className="mx-2">{item.quantity}</span>
+                  <Button variant="danger" onClick={() => handleRemoveItem(item.id)} disabled={item.quantity <= 1}>-</Button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Modal.Body>
+        <Modal.Footer>
+          <p className="text-primary">Total: Rs. {getTotalPrice()}</p>
+          <Button variant="primary">Checkout</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,29 @@
-import React, { useState } from 'react';
-import { Navbar, Container, Button, Badge } from 'react-bootstrap';
-import Cart from './Cart'; // Import the Cart component
-
+import React, { useState } from "react";
+import { Navbar, Container, Button, Badge } from "react-bootstrap";
+import Cart from "./Cart"; // Import the Cart component
+//icons
+import ShopyIcon from './assets/Shopy.png';
+import CartIcon from './assets/Cart.png';
 const Header = () => {
   const [showCart, setShowCart] = useState(false);
   const cartItemsCount = 5; // Replace with your actual cart item count
 
   const toggleCart = () => {
-    setShowCart(prevShowCart => !prevShowCart);
+    setShowCart((prevShowCart) => !prevShowCart);
   };
 
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar style={{ backgroundColor: '#1d3b55', fontFamily: "cursive"}}>
       <Container>
-        <Navbar.Brand href="#home">Shopy</Navbar.Brand>
-        <Button onClick={toggleCart} style={{backgroundColor: '#0e416c'}}> 
-           My Cart
-          <Badge style={{backgroundColor: '#dcedf9'}} className="ml-1">
-            {cartItemsCount}
-          </Badge>
-        </Button>
+        
+        <Navbar.Brand href="#home" style={{color: 'White', fontFamily: "cursive"}}><img src={ShopyIcon} alt="icon" style={{width:'5vw', height: '7vh'}}/>Shopy</Navbar.Brand>
+        <Button onClick={toggleCart} style={{backgroundColor: '#3e5c74'}}>
+        <img src={CartIcon} alt="cart-icon" style={{width:'3vw', height: '5vh'}}/>My Cart
+  <Badge bg="info" style={{marginLeft: "1rem"}}>
+    {cartItemsCount}
+  </Badge>
+</Button>
+
         {showCart && <Cart />}
       </Container>
     </Navbar>
